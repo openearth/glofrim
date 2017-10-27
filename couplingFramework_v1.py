39200fd671fce5e5c34b6a66e583dbe81dd32006
ChippChapp <j.m.hoch@uu.nl>
2017-10-27 13:30:45 +0200

    updated code

--- couplingFramework_v1.py
+++ couplingFramework_v1.py
@@ -142,25 +142,16 @@ clone_pcr        	=  config.PCR_settings['clone_pcr']
 
 # these may be changed according to personal file and folder structure
 if model_type == 'DFM':
-<<<<<<< HEAD
     if platform.system() == 'Linux':
-        model_path = '/home/jannis/Programmes/DFLOWFM/lib/libdflowfm.so'  # for Linux
+        model_path = '/path/to/DFLOWFM/lib/libdflowfm.so'  # for Linux
     elif platform.system() == 'Windows':
          model_path = '/path/to/DFLOWFM/lib/libdflowfm.dll'  # for Windows
 
 elif model_type == 'LFP':
     if platform.system() == 'Linux':
-        model_path = '/home/jannis/Programmes/LISFLOODFP/lisflood-bmi-v5.9/liblisflood.so'  # for Linux
+        model_path = '/path/to/lisflood-bmi-v5.9/liblisflood.so'  # for Linux
     elif platform.system() == 'Windows':
-        sys.exit('\nLFP v5.9 with BMI currently not supported on Windows - sorry!\n')
-
-=======
-    model_path 			= '/path/to/DFLOWFM/lib/libdflowfm.so'
-elif model_type == 'LFP':
-    model_path 			= '/path/to/lisflood-bmi-v5.9/liblisflood.so' 
->>>>>>> 0b6bd50385ddac1b2f4198fb32ee2320ebc8c95a
-else:
-    sys.exit('\nno adequate model defined in ini-file - define either FM or FP!\n')
+        sys.exit('\nLFP v5.9 with BMI currently not supported on Windows!\n')
 
 # -------------------------------------------------------------------------------------------------
 # INITIALIZE AND SPIN-UP PCR-GLOBWB
