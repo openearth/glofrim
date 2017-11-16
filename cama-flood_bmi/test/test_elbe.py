43d0c8491982346677c045700e420fa6621e93a1
Hiroaki Ikeuchi <ikeuchi@Hiroaki-MacBookPro.local>
2017-11-16 15:28:23 +0100

    exposed runoff input variable

--- cama-flood_bmi/test/test_elbe.py
+++ cama-flood_bmi/test/test_elbe.py
@@ -12,10 +12,18 @@ if __name__ == "__main__":
     model.set_logger(logger)
     model.initialize("../../test_Elbe/CFM_Elbe/")
 
-    #TODO: UPDATE RUNOFF HERE
+    #TODO: UPDATE RUNOFF HERE, later obtained from PCR
     sample_runoff = np.fromfile('../../sample_runoff/ELSE_GPCC/Roff/Roff____19900101.one', 'f').reshape(7,10)
+#    print(sample_runoff)
     model.set_var('runoff', sample_runoff)
-    print runoff
+
+# Check updated runoff
+#    runoff = model.get_var('runoff').reshape(7,10)
+#    print(runoff)
+#    print(model.get_var_shape('runoff'))
+#    diff = sample_runoff - runoff
+#    print(diff.max(), diff.min())
+#    sys.exit()
 
     start_time = model.get_start_time()
     current_time = model.get_current_time()
@@ -38,31 +46,7 @@ if __name__ == "__main__":
         time_step
     )
 
-    logger.info("type: %s", model.get_var_type('sfcelv'))
-    logger.info("rank: %s", model.get_var_rank('sfcelv'))
-    logger.info("shape: %s", model.get_var_shape('sfcelv'))
-    logger.info("sfcelv: %s", model.get_var('sfcelv'))
-
-    names =[
-        "rivinf", "rivdph", "rivvel", "fldinf",
-        "flddph", "fldfrc", "fldare", "pthout",
-        "pthinf", "sfcelv", "outflw", "storge",
-        "rivout_avg", "outflw_avg", "fldout_avg",
-        "rivvel_avg", "pthout_avg", "pthflw_avg"
-    ]
-    for name in names[:2]:
-        arr = model.get_var(name)
-        logger.info("variable %s:\n%s", name, arr)
-
-    model.update(10)
-    before = model.get_var('sfcelv')[0, 0]
-    arr = model.get_var('sfcelv').copy()
-    arr[0] += 1
-    model.set_var('sfcelv', arr)
-    after = model.get_var('sfcelv')[0, 0]
-    logger.info("before %s, after %s, ok: %s", before, after, (after - before) == 1)
-    # logger.info("sfcelv: %s", model.get_var('sfcelv'))
-
+# For the model runs, input runoff should be updated from PCR by set_var function
 #    while model.get_current_time() < model.get_end_time():
 #        model.update(model.get_time_step())
     model.finalize()
