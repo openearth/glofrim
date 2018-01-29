1d65d7748e696b02031771939d1faccddc3003cb
Dirk Eilander <dirk.eilander@gamil.com>
2018-01-29 13:00:17 +0100

    some improvements to BMI model wrappers

--- coupling/coupling_PCR_FM/model_functions_v2.py
+++ coupling/coupling_PCR_FM/model_functions_v2.py
@@ -3,6 +3,7 @@ import numpy as np
 from pcrglobwb_bmi_v203 import pcrglobwb_bmi
 from bmi.wrapper import BMIWrapper
 import logging
+import warnings
 
 logger = logging.getLogger(__name__)
 
@@ -13,9 +14,9 @@ class _model(object):
         self.name = name
         self.mtype = mtype
         self.missing_value = missing_value
-        # self.start_time = self.bmi.ge()
+        self.start_time = self.get_start_time()
 
-    def get_var(self, var, parse_missings=True):
+    def get_var(self, var, parse_missings=True, *args, **kwargs):
         var_data = self.bmi.get_var(var)
         if parse_missings:
             # if given nodata is parsed to np.nan
@@ -28,13 +29,27 @@ class _model(object):
     def spinup(self, *args, **kwargs):
         self.bmi.spinup(*args, **kwargs)
 
-    def update(self, *args, **kwargs):
-        self.bmi.update(*args, **kwargs)
-        current_time = self.bmi.get_current_time()
+    def get_start_time():
+        "get model start (initialization) time"
+        self.bmi.get_start_time()
+
+    def get_current_time():
+        "get model current time"
+        self.bmi.get_start_time()
+
+    def get_time_step():
+        "get model current time step"
+        self.bmi.get_start_time()
+
+    def update(self, dt=-1):
+        self.bmi.update(dt=dt)
+        start_time = self.start_time
+        current_time = self.get_current_time()
         time_step = self.bmi.get_time_step()
         logger.info(
-            "start_time: %s, current_time %s, timestep %s",
-            current_time, #TODO replace by self.start_time
+            "%s -> start_time: %s, current_time %s, timestep %s",
+            self.name,
+            start_time, #TODO replace by self.start_time
             current_time,
             time_step
         )
@@ -47,18 +62,64 @@ class PCR_model(_model):
         super(PCR_model, self).__init__(pcr_bmi, 'PCRGLOB-WB', 'hydrology',
                                         missing_value=missing_value)
 
+    def run(self, dt=1):
+        "run model for dt [days]"
+        super(PCR_model, self).update(dt=dt)
 
 class CMF_model(_model):
-    def __init__(self, path_to_model, configfile, map_dir):
-        cmf_bmi = BMIWrapper(engine = path_to_model, configfile = configfile)
+    def __init__(self, engine, configfile, map_dir, get_runoff=None):
+        """GLOFRIM wrapper around BMI for CaMa-Flood (CMF)
+
+        input
+        engine          BMIWrapper path to CMF engine
+        configfile      BMIWrapper CMF config file name
+        map_dir         CMF map directory. Required to initialize CMF
+        get_runoff      Function to get runoff from upstream coupled model.
+                        This can also be set later using the set_options function.
+
+        """
+        cmf_bmi = BMIWrapper(engine = engine, configfile = configfile)
         cmf_bmi.initialize(map_dir)
         super(CMF_model, self).__init__(cmf_bmi, 'CaMa-Flood', 'hydrodynamic')
+        self.get_runoff = get_runoff
 
-    def update(self, runoff, *args, **kwargs):
-        runoff = np.where(np.isnan(runoff), 0, runoff)
-        self.set_var('runoff', runoff)
-        super(CMF_model, self).update(*args, **kwargs)
+    def set_options(self, get_runoff=None):
+        if get_runoff is not None:
+            self.get_runoff = get_runoff
+            logger.info('CMF get runoff function set')
 
+
+    def run(self, dt=1):
+        "update runoff state and run for dt [days]"
+        dt = dt * 86400 # convert days to seconds
+        if self.get_runoff is not None:
+            self.set_var('runoff',  self.get_runoff())
+            super(CMF_model, self).update(dt=dt)
+        else:
+            warnings.warn(
+                "set get_runoff function first using the set_options function",
+                RuntimeWarning
+            )
+
+    def get_var(self, var, parse_missings=True, *args, **kwargs):
+        var = super(CMF_model, self).get_var(var, parse_missings=parse_missings,
+                                             *args, **kwargs)
+        # return var with switched axis (fortran to python translation)
+        return var.reshape(var.shape[::-1])
+
+    def _CMFtime_2_datetime(self, t, yr0=1850):
+        # internal CMF time is in minutes since 1850
+        h, m = divmod(t, 60)
+        d, h = divmod(h, 24)
+        return datetime(yr0, 1, 1) + timedelta(days = d)
+
+    def get_current_time(self):
+        t = super(CMF_model, self).get_current_time()
+        return self._CMFtime_2_datetime(t)
+
+    def get_start_time(self):
+        t = super(CMF_model, self).get_start_time()
+        return self._CMFtime_2_datetime(t)
 
 # class DFM_model(_model):
 #     def __init__(self, path_to_model, configfile):
