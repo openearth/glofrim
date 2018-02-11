ae43e5cd743bb48956da0749c1517c93833b4a77
dirk.eilander@gmail.com <dirk.eilander@gamil.com>
2018-02-11 19:32:47 +0100

    DFM coupling and two stage initialization

--- coupling/coupling_PCR_FM/configuration.py
+++ coupling/coupling_PCR_FM/configuration.py
@@ -4,7 +4,7 @@ Created on Wed Jan 20 13:32:04 2016
 
 This set of functions executes the actual model functions to manipulate model
 arrays and to compute the required volumes and depths, as well as updating
-water heights accordingly. 
+water heights accordingly.
 
 Please note: currently only to be used for 1way-coupling!!!
 
@@ -17,13 +17,13 @@ import os
 import optparse
 
 class Configuration(object):
-    
+
     def __init__(self):
-        
+
         object.__init__(self)
-        
+
     def parse_configuration_file(self, modelFileName):
-        
+
         config = ConfigParser.ConfigParser()
         config.optionxform = str
         config.read(modelFileName)
@@ -31,10 +31,10 @@ class Configuration(object):
         # all sections provided in the configuration/ini file
         self.allSections  = config.sections()
 
-        # read all sections 
+        # read all sections
         for sec in self.allSections:
-            vars(self)[sec] = {}                               # example: to instantiate self.globalOptions 
+            vars(self)[sec] = {}                               # example: to instantiate self.globalOptions
             options = config.options(sec)                      # example: logFileDir
             for opt in options:
-                val = config.get(sec, opt)                     # value defined in every option 
+                val = config.get(sec, opt)                     # value defined in every option
                 self.__getattribute__(sec)[opt] = val          # example: self.globalOptions['logFileDir'] = val
