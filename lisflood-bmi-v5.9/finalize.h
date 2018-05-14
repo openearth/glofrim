7b6bb29f601dbb11ab96fe4aaa06260ce486253c
Jannis Hoch <j.m.hoch@uu.nl>
2018-05-14 12:58:52 +0200

    removed LISFLOOD-FP-BMI model; code can be found in new GitLab repos now

--- lisflood-bmi-v5.9/finalize.h
+++
@@ -1,9 +0,0 @@
-#ifndef FINALIZE_H
-#define FINALIZE_H
-
-#include "global.h"
-
-void final_iterateq();
-void final();
-
-#endif /* FINALIZE_H */
--- lisflood-bmi-v5.9/update.h
+++
@@ -1,8 +0,0 @@
-#ifndef UPDATE_H
-#define UPDATE_H
-
-#include "global.h"
-
-void iterateq_step();
-
-#endif /* UPDATE_H */
