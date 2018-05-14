e02aca4539183fe96b2a7b7093ae3f776a22ab9e
Jannis Hoch <j.m.hoch@uu.nl>
2018-05-14 12:52:17 +0200

    first baby steps to implement LFP

--- glofrim-py/glofrim/__init__.py
+++ glofrim-py/glofrim/__init__.py
@@ -1,5 +1,6 @@
 from glofrim_cmf import CMF_model
 from glofrim_dfm import DFM_model
+from glofrim_lfp import LFP_model
 from glofrim_pcr import PCR_model
 from glofrim_wfl import WFL_model
 # from glofrim_lfp import LFP_model
