bf1852116c1b6b8080bbbeae7a283a9099d298a1
Jannis <j.m.hoch@uu.nl>
2018-08-15 14:16:42 +0200

    now everything is available via readthedocs and also added some first tests with autodoc and autosummary

--- conf.py
+++ conf.py
@@ -36,7 +36,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.mathjax',
-    'IPython.sphinxext.ipython_console_highlighting']
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
