c10704c8e1f8d5244f0d56815347975d0c3296fc
DirkEilander <dirk.eilander@gmail.com>
2018-08-23 20:21:47 +0000

    improved get_grid for PCR and CMF; small bugfix in grids

--- glofrim-py/glofrim/pcr_bmi.py
+++ glofrim-py/glofrim/pcr_bmi.py
@@ -343,14 +343,21 @@ class PCR(GBmi):
             # ldd is used for coupling to routing / flood model
             _indir = abspath(self.get_attribute_value('globalOptions:inputDir'))
             _ldd_fn = glib.getabspath(self.get_attribute_value('routingOptions:lddMap'), _indir)
-            if not isfile(_ldd_fn): raise IOError('ldd file not found')
+            if not isfile(_ldd_fn): raise IOError('ldd file not found {}'.format(_ldd_fn))
             # landmask used for masking out coordinates outside mask
             _lm_fn = glib.getabspath(self.get_attribute_value('globalOptions:landmask'), _indir)
-            if not isfile(_lm_fn): raise IOError('landmask file not found')
-            self.logger.info('Getting rgrid info based on {}'.format(basename(_lm_fn)))
-            with rasterio.open(_lm_fn, 'r') as ds:
-                self.grid = RGrid(ds.transform, ds.height, ds.width, crs=ds.crs, mask=ds.read(1)==1)
-
+            if isfile(_lm_fn): #raise IOError('landmask file not found')
+                with rasterio.open(_lm_fn, 'r') as ds:
+                    mask=ds.read(1)==1
+                    mask_name = basename(_lm_fn)
+            else:
+                mask=None
+                mask_name = basename(_ldd_fn)
+            self.logger.info('Getting rgrid info based on {}; mask based on {}'.format(basename(_ldd_fn), mask_name))
+            with rasterio.open(_ldd_fn, 'r') as ds:
+                if mask is None:
+                    mask=ds.read(1) != ds.nodata
+                self.grid = RGrid(ds.transform, ds.height, ds.width, crs=ds.crs, mask=mask)
             # read file with pcr readmap
             self.logger.info('Getting drainage direction from {}'.format(basename(_ldd_fn)))
             self.grid.set_dd(_ldd_fn, ddtype='ldd', nodata=255)
