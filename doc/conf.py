23ef4a35c065abfc4b575cb8a59c127b02a0a8a4
Jannis <j.m.hoch@uu.nl>
2018-09-03 11:18:58 +0200

    updated logo, it is superduper cool now

--- doc/conf.py
+++ doc/conf.py
@@ -133,7 +133,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_images/logo.png"
+html_logo = "_images/GLOFRIMlogo2.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
